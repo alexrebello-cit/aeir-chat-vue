c78e9725e2ce555ba19482626e24bf5f20907a7d
m537157 <alex.rebello@bradesco.com.br>
2025-11-14 16:19:21 -0300

    auth tests

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import outputs from '../amplify_outputs.json';
+
+  Amplify.configure(outputs);
 </script>
 
 <template>
+  
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+
   <main>
     <Todos />
   </main>
+
 </template>
 
